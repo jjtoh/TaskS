f1fe40ab73881362c77ac37bd3383fe2a6db587b
jjtoh <johntohjiajun@gmail.com>
2022-09-16 17:27:22 +0800

    Add some methods

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -36,6 +36,14 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count();
+
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
         System.out.println("Printing data using loops:");
         for (Task t : tasksData) {
@@ -43,11 +51,25 @@ public class Main {
         }
     }
 
+    public static void printDataUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing data using streams");
+        tasks.stream()
+                .forEach(System.out::println);
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using loops:");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing Deadline using streams:");
+        tasks.parallelStream()
+                .filter(t -> t instanceof Deadline) //lambda
+                .forEach(System.out::println);
     }
 }
