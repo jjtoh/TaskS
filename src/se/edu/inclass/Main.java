82f999339c41737d1251302c374962a12a20aec6
jjtoh <johntohjiajun@gmail.com>
2022-09-16 17:48:37 +0800

    Add lamda methods

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -6,6 +6,9 @@ import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -18,10 +21,13 @@ public class Main {
 //        System.out.println("All data:");
 //        printData(tasksData);
 
-        System.out.println("Printing deadlines");
+        System.out.println("Printing deadlines using loop");
         printDeadlines(tasksData);
+        printDeadlinesUsingStream(tasksData);
 
-
+        ArrayList<Task> filteredList = filterTaskByString(tasksData, "11");
+        System.out.println("Data with string 11:");
+        printData(filteredList);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
     }
@@ -48,5 +54,20 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Print sorted deadlines:");
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().compareToIgnoreCase(b.getDescription()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterTaskByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredList;
     }
 }
